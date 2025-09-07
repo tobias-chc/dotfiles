4fe7b4320b165d68a9f89c0c6cccafc4beaf1c38
Tobías Chavarría <tobias.chavarria.castro@gmail.com>
2025-09-07 22:13:31 +0200

    feat(nvim): add snippets and new options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 require 'core.keymaps'
 require 'core.options'
+require 'core.snippets'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
